7379855137f523f6a40a0c9ae4a7699c2273a5a3
adnansyed101 <sirgeant10@gmail.com>
2026-01-18 17:09:37 +0600

    Complete homepage.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,28 @@
+import BestWorkersSection from '@/components/home/best-workers-section'
+import CTASection from '@/components/home/cta-section'
+import FeaturesSection from '@/components/home/features-section'
+import HeroSection from '@/components/home/hero-section'
+import HowItWorksSection from '@/components/home/how-it-works-section'
+import TaskCategoriesSection from '@/components/home/task-categories-section'
+import TestimonialsSection from '@/components/home/testimonials-section'
+import Navbar from '@/components/layout/navbar'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/')({ component: App })
 
 function App() {
-  return <h1>Hello world</h1>
+  return (
+    <div className="min-h-screen">
+      <Navbar />
+      <main>
+        <HeroSection />
+        <BestWorkersSection />
+        <HowItWorksSection />
+        <TaskCategoriesSection />
+        <TestimonialsSection />
+        <FeaturesSection />
+        <CTASection />
+      </main>
+    </div>
+  )
 }
