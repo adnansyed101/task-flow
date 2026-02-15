7c5348cd545cfea189ff16e3f064b2b2cc0fa993
adnansyed101 <sirgeant10@gmail.com>
2026-02-15 15:41:04 +0600

    Remove page.

--- src/routes/dashboard/tasks.tsx
+++
@@ -1,9 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-
-export const Route = createFileRoute('/dashboard/tasks')({
-  component: TaskPage,
-})
-
-function TaskPage() {
-  return <div>Hello "/dashboard/tasks"!</div>
-}
