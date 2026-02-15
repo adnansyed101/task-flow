fd15cd56cc711f336880df5911d3a14485a36d4b
adnansyed101 <sirgeant10@gmail.com>
2026-02-15 15:27:55 +0600

    Add not found component to page.

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,6 @@
 import {
   HeadContent,
+  Link,
   Scripts,
   createRootRouteWithContext,
 } from '@tanstack/react-router'
@@ -38,6 +39,7 @@ export const Route = createRootRouteWithContext<MyRouterContext>()({
   }),
 
   shellComponent: RootDocument,
+  notFoundComponent: NotFound,
 })
 
 function RootDocument({ children }: { children: React.ReactNode }) {
@@ -64,5 +66,21 @@ function RootDocument({ children }: { children: React.ReactNode }) {
         <Scripts />
       </body>
     </html>
+  )
+}
+
+function NotFound() {
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-muted">
+      <div className="text-center">
+        <h1 className="mb-4 text-4xl font-bold">404</h1>
+        <p className="mb-4 text-xl text-muted-foreground">
+          Oops! Page not found
+        </p>
+        <Link to="/" className="text-primary underline hover:text-primary/90">
+          Return to Home
+        </Link>
+      </div>
+    </div>
   )
 }
