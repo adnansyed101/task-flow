cde594fe7ab1bd36706aa1497492e33f541386f6
adnansyed101 <sirgeant10@gmail.com>
2026-07-11 20:48:10 +0600

    Add constant for registration.

---
+++ src/lib/constants.ts
@@ -0,0 +1,10 @@
+import type { RegistrationType } from './schema/registration'
+
+export const registrationConstant: RegistrationType = {
+  fullName: '',
+  email: '',
+  password: '',
+  image:
+    'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQNY9Yi56KsGN7_18bLNltEhs7GP1q8e_UBD24GzpQyYpxpcEOA22FzmFM&s=10',
+  role: 'worker',
+}
