0ca4471669059647a71b25f73f2437aea31154ad
adnansyed101 <sirgeant10@gmail.com>
2026-07-11 20:47:43 +0600

    Create Registration validator.

---
+++ src/lib/schema/registration.ts
@@ -0,0 +1,11 @@
+import z from 'zod'
+
+export const registrationSchema = z.object({
+  image: z.string().url('Invalid image URL').optional(),
+  fullName: z.string().min(3, 'Full Name must be at least 3 characters long'),
+  email: z.string().email('Invalid email address'),
+  password: z.string().min(6, 'Password must be at least 6 characters long'),
+  role: z.enum(['worker', 'buyer']),
+})
+
+export type RegistrationType = z.infer<typeof registrationSchema>
