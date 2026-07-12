15378afd1069be1204cee266f8be419b0a8c8723
adnansyed101 <sirgeant10@gmail.com>
2026-07-12 14:30:47 +0600

    Create login schema.

--- src/lib/schema/registration.ts
+++ src/lib/schema/registration.ts
@@ -9,3 +9,10 @@ export const registrationSchema = z.object({
 })
 
 export type RegistrationType = z.infer<typeof registrationSchema>
+
+export const loginSchema = z.object({
+  email: z.string().email('Invalid email address'),
+  password: z.string().min(6, 'Password must be at least 6 characters long'),
+})
+
+export type LoginType = z.infer<typeof loginSchema>
