eb5742ba405e04e3b86cbbe1f6e9f62514212b93
adnansyed101 <sirgeant10@gmail.com>
2026-07-13 14:22:29 +0600

    Create task schema.

---
+++ src/lib/schema/task.ts
@@ -0,0 +1,29 @@
+import { z } from 'zod'
+import { customUserSchema } from './user'
+
+const baseTaskSchema = z.object({
+  id: z.string(),
+  taskTitle: z.string(),
+  taskDetail: z.string(),
+  requiredWorkers: z.number(),
+  payableAmount: z.number(),
+  completionDate: z.date({ error: 'Date of birth is required' }),
+  submissionInfo: z.string(),
+  taskImageUrl: z.string(),
+  status: z.union([
+    z.literal('pending'),
+    z.literal('approved'),
+    z.literal('rejected'),
+  ]),
+})
+
+export const FormTaskSchema = baseTaskSchema.extend({
+  buyerId: z.string().min(1, 'Buyer is required'),
+})
+
+export const ResponseTaskSchema = baseTaskSchema.extend({
+  buyer: customUserSchema,
+})
+
+export type FormTaskValuesType = z.infer<typeof FormTaskSchema>
+export type ResponseTaskType = z.infer<typeof ResponseTaskSchema>
