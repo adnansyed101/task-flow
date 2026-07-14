197f804deb32acf0370798137a98c34946571ced
adnan <sirgeant10@gmail.com>
2026-07-14 23:12:58 +0600

    Create get request.

--- src/routes/api/task/index.tsx
+++ src/routes/api/task/index.tsx
@@ -2,10 +2,48 @@ import { FormTaskSchema } from '#/lib/schema/task'
 import { createFileRoute } from '@tanstack/react-router'
 import { prisma } from '#/db'
 import z from 'zod'
+import { ensureSession } from '#/middleware/auth.function'
 
 export const Route = createFileRoute('/api/task/')({
   server: {
     handlers: {
+      GET: async () => {
+        const session = await ensureSession()
+        try {
+          const tasks = await prisma.task.findMany({
+            where: {
+              buyerId: session.user.id,
+            },
+          })
+
+          return Response.json({
+            success: true,
+            data: tasks,
+            message: 'Task Created Successfully.',
+          })
+        } catch (error) {
+          // z.parse() throws a ZodError if validation fails
+          if (error instanceof z.ZodError) {
+            return Response.json(
+              {
+                success: false,
+                error: error.issues,
+                message: 'Zod Error',
+              },
+              { status: 400 },
+            )
+          }
+
+          // Handle database or other unexpected errors
+          return Response.json(
+            {
+              success: false,
+              error: 'Internal server error in creating tasks.',
+            },
+            { status: 400 },
+          )
+        }
+      },
       POST: async ({ request }) => {
         try {
           const body = await request.json()
@@ -47,6 +85,7 @@ export const Route = createFileRoute('/api/task/')({
       PATCH: async ({ request }: { request: Request }) => {
         try {
           const body = await request.json()
+          body.completionDate = new Date(body.completionDate)
           const vlaidatedData = FormTaskSchema.parse(body)
 
           const newTask = await prisma.task.update({
@@ -59,7 +98,7 @@ export const Route = createFileRoute('/api/task/')({
           return Response.json({
             success: true,
             data: newTask,
-            message: 'Task Created Successfully.',
+            message: 'Task Updated Successfully.',
           })
         } catch (error: any) {
           // z.parse() throws a ZodError if validation fails
