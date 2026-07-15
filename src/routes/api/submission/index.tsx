52913f65e7d1815e5fb31ea431b52a6b7f9e20fb
adnansyed101 <sirgeant10@gmail.com>
2026-07-15 13:37:44 -0700

    Add patch handler  to submission.

--- src/routes/api/submission/index.tsx
+++ src/routes/api/submission/index.tsx
@@ -1,6 +1,16 @@
 import { prisma } from '#/db'
 import { ensureSession } from '#/middleware/auth.function'
 import { createFileRoute } from '@tanstack/react-router'
+import z from 'zod'
+
+const submissionSchema = z.object({
+  status: z.union([
+    z.literal('pending'),
+    z.literal('approved'),
+    z.literal('rejected'),
+  ]),
+  id: z.string(),
+})
 
 export const Route = createFileRoute('/api/submission/')({
   server: {
@@ -15,6 +25,7 @@ export const Route = createFileRoute('/api/submission/')({
               task: {
                 buyerId: session.user.id,
               },
+              status: 'pending',
             },
             include: {
               task: true,
@@ -33,6 +44,75 @@ export const Route = createFileRoute('/api/submission/')({
             {
               success: false,
               error: 'Internal server error in fetching submissions.',
+            },
+            { status: 400 },
+          )
+        }
+      },
+      PATCH: async ({ request }) => {
+        const body = await request.json()
+
+        const validatedData = submissionSchema.parse(body)
+
+        try {
+          const updatedSubmission = await prisma.$transaction(async (tx) => {
+            const updateSubmission = await tx.submission.update({
+              where: {
+                id: validatedData.id,
+              },
+              data: {
+                status: validatedData.status,
+              },
+              include: {
+                task: true,
+              },
+            })
+
+            if (validatedData.status === 'rejected') {
+              await tx.task.update({
+                where: {
+                  id: updateSubmission.taskId,
+                },
+                data: {
+                  requiredWorkers: {
+                    increment: 1,
+                  },
+                },
+              })
+            }
+
+            if (validatedData.status === 'approved') {
+              await tx.user.update({
+                where: {
+                  id: updateSubmission.workerId,
+                },
+                data: {
+                  coin: {
+                    increment: updateSubmission.task.payableAmount,
+                  },
+                },
+              })
+            }
+
+            return updateSubmission
+          })
+
+          return Response.json({
+            success: true,
+            data: updatedSubmission,
+            message: 'Submission Updated Successfully.',
+          })
+        } catch (error) {
+          // z.parse() throws a ZodError if validation fails
+          if (error instanceof z.ZodError) {
+            return Response.json({ success: false, error: error.issues })
+          }
+
+          // Handle database or other unexpected errors
+          return Response.json(
+            {
+              success: false,
+              error: 'Internal server error in updating submission.',
             },
             { status: 400 },
           )
